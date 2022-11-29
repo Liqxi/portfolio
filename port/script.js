b4f46f98f964a3cab10e17eb6820ebf0ae71722a
Liqxi <117170555+Liqxi@users.noreply.github.com>
2022-11-29 16:38:29 +0600

    Add files via upload

---
+++ port/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
+
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  //typing script
+  var typed = new Typed(".typing", {
+    strings: ["Web Dev", "Gamer", "Student"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  $(".a").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+});
+function preloader() {
+  $(document).ready(function () {
+    $("#overflow").css("overflow", "hidden");
+    setTimeout(function load() {
+      let loading = $(".preloader");
+      loading.css("display", "none");
+      $("#overflow").css("overflow", "auto");
+    }, 1500);
+  });
+}
+preloader();
